60990cc29637d0d5aad5c739bad4f9524e141f36
Yaguedid2 <110475421+Yaguedid2@users.noreply.github.com>
2022-08-27 12:00:19 +0100

    Add files via upload

---
+++ MoveRacket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    public float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+     }
+}
